03593056c56ed465f4369114063526c656329955
Md Sirajul Munir <abc@gmail.com>
2023-09-17 08:58:16 +0600

    fake data ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Blogs from './Components/Blogs/Blogs'
+import Header from './Components/Header/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-     <h2 className='text-3xl bg-red-300'>Knowledge Cafe</h2>
+     <Header></Header>
+     <Blogs></Blogs>
     </>
   )
 }
